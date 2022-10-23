c672aaa1e11c82e027376e72dcba93e86f968ec2
IgnacioTomasRD <iruizdiaz@frba.utn.edu.ar>
2022-10-23 11:38:21 -0300

    spring

---
+++ src/main/java/webapp/main.java
@@ -0,0 +1,11 @@
+package webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+}
